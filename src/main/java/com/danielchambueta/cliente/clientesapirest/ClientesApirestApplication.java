fb2d90c44dc2661dbcf0385ee933afaff79dc14a
Daniel Chambueta <83682738+DanielChambueta@users.noreply.github.com>
2024-05-31 12:32:25 -0500

    First commit

---
+++ src/main/java/com/danielchambueta/cliente/clientesapirest/ClientesApirestApplication.java
@@ -0,0 +1,13 @@
+package com.danielchambueta.cliente.clientesapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientesApirestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danielchambueta/cliente/clientesapirest/ClientesApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielchambueta.cliente.clientesapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
