6ddb9d8bb7e73a39249780196166942d4c4e0f36
Daniel Chambueta <83682738+DanielChambueta@users.noreply.github.com>
2024-06-06 12:09:28 -0500

    DOCUMENT - Capa de Servicio

--- src/main/java/com/danielchambueta/cliente/clientesapirest/service/ClienteService.java
+++ src/main/java/com/danielchambueta/cliente/clientesapirest/service/ClienteService.java
@@ -5,6 +5,10 @@ import com.danielchambueta.cliente.clientesapirest.model.entity.Cliente;
 
 import java.util.List;
 
+/**
+ * Interfaz para el servicio de la entidad Cliente.
+ * Define los métodos para el manejo de la entidad Cliente en la capa de servicio.
+ */
 public interface ClienteService {
 
     List<Cliente> listAll();
--- src/main/java/com/danielchambueta/cliente/clientesapirest/service/implement/ClienteImplementationService.java
+++ src/main/java/com/danielchambueta/cliente/clientesapirest/service/implement/ClienteImplementationService.java
@@ -10,19 +10,38 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * Clase que implementa la interfaz ClienteService.
+ * Proporciona la implementación de los métodos definidos en la interfaz ClienteService.
+ * Utiliza la anotación @Service para indicar que es una clase de servicio en Spring.
+ * Utiliza la anotación @Autowired para la inyección de dependencias de la interfaz ClienteDao.
+ * Utiliza la anotación @Transactional para la gestión de transacciones en los métodos que modifican la base de datos.
+ *
+ */
 @Service
 public class ClienteImplementationService implements ClienteService {
 
+    /**
+     * Inyección de dependencias de la interfaz ClienteDao.
+     */
     @Autowired
     private ClienteDao clienteDao;
 
+    /**
+     * Método para obtener todos los clientes.
+     * @return una lista de clientes.
+     */
     @Override
     public List<Cliente> listAll() {
         return (List) clienteDao.findAll();
     }
 
+    /**
+     * Método para guardar un cliente.
+     * @param clienteDto objeto ClienteDto con los datos del cliente a guardar.
+     * @return el cliente guardado.
+     */
     @Transactional
-
     public Cliente save(ClienteDto clienteDto) {
         Cliente cliente = Cliente.builder()
                 .idCliente(clienteDto.getIdCliente())
@@ -34,18 +53,32 @@ public class ClienteImplementationService implements ClienteService {
         return clienteDao.save(cliente);
     }
 
+    /**
+     * Método para buscar un cliente por su identificador.
+     * @param id el identificador del cliente.
+     * @return el cliente encontrado.
+     */
     @Transactional(readOnly = true)
     @Override
     public Cliente findById(Integer id) {
         return clienteDao.findById(id).orElse(null);
     }
 
+    /**
+     * Método para eliminar un cliente.
+     * @param cliente el cliente a eliminar.
+     */
     @Transactional
     @Override
     public void delete(Cliente cliente) {
         clienteDao.delete(cliente);
     }
 
+    /**
+     * Método para verificar si un cliente existe por su identificador.
+     * @param id el identificador del cliente.
+     * @return verdadero si el cliente existe, falso en caso contrario.
+     */
     @Override
     public boolean existsById(Integer id) {
         return clienteDao.existsById(id);
